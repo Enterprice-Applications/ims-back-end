4fa397e0e280c9e3d222673b7fd239cf03cd1b8a
PrasadPrabash <prasad.lakman@gmail.com>
2023-11-22 17:58:57 +0530

    Implement TeacherHttpController and test

--- src/main/java/lk/ijse/dep11/ims/api/TeacherHttpController.java
+++ src/main/java/lk/ijse/dep11/ims/api/TeacherHttpController.java
@@ -1,11 +1,46 @@
 package lk.ijse.dep11.ims.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.dep11.ims.to.TeacherTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/teachers")
 public class TeacherHttpController {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public TeacherTO createTeacher(@RequestBody @Validated TeacherTO details){
+        System.out.println("Create Teacher");
+        return new TeacherTO();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping(value ="/{teacherId}", consumes = "application/json")
+    public void updateTeacher(@PathVariable("teacherId") String teacherID,
+                              @RequestBody @Validated TeacherTO teacherUpdate){
+        System.out.println("update Teacher");
+    }
+
+    @GetMapping(produces = "application/json")
+    public List<TeacherTO> getAllTeacher(){
+        System.out.println("getAllTeacher");
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value ="/{teacherId}", produces = "application/json")
+    public void getTeacher(){
+        System.out.println("getTeacher");
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{teacherId}")
+    public void deleteTeacher(@PathVariable String teacherId){
+        System.out.println("Delete Teacher");
+    }
 }
