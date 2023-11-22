7e23fbdfedf4ee2f2636da143153c8b6288640dc
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-11-22 17:52:00 +0530

    Add Post mapping

--- src/main/java/lk/ijse/dep11/ims/api/CourseHttpController.java
+++ src/main/java/lk/ijse/dep11/ims/api/CourseHttpController.java
@@ -1,11 +1,17 @@
 package lk.ijse.dep11.ims.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.dep11.ims.to.CourseTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/course")
 @CrossOrigin
 public class CourseHttpController {
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = "application/json",consumes = "application/json")
+    public CourseTO createTask(@RequestBody @Validated(CourseTO.Create.class) CourseTO course){
+        return course;
+    }
 }
---
+++ src/main/java/lk/ijse/dep11/ims/to/CourseTO.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep11.ims.to;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseTO implements Serializable {
+    private Integer id;
+    private String name;
+    private Integer duration_in_moth;
+
+    public class Create {
+    }
+}
