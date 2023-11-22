a70cd87619d6a57b960a35bcbd246bf9a321d902
PrasadPrabash <prasad.lakman@gmail.com>
2023-11-23 00:34:14 +0530

    Add Regular expressions for backend validation

--- src/main/java/lk/ijse/dep11/ims/to/TeacherTO.java
+++ src/main/java/lk/ijse/dep11/ims/to/TeacherTO.java
@@ -7,6 +7,7 @@ import org.springframework.lang.Nullable;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Null;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
 @Data
@@ -16,7 +17,9 @@ public class TeacherTO implements Serializable {
     @Null(message = "Id should be empty")
     private Integer id;
     @NotBlank(message = "name should not be empty")
+    @Pattern(regexp = "^[A-Za-z ]+$", message = "Invalid name")
     private String name;
     @NotBlank(message = "contact should not be empty")
+    @Pattern(regexp = "^\\d{3}-\\d{7}$", message = "Invalid contact")
     private String contact;
 }
