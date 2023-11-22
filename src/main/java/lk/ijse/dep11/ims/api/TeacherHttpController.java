c7b89b37edba66f5dcea62489a5857cdb6c9ff2f
PrasadPrabash <prasad.lakman@gmail.com>
2023-11-22 19:30:48 +0530

    create update Teacher POST request

--- src/main/java/lk/ijse/dep11/ims/api/TeacherHttpController.java
+++ src/main/java/lk/ijse/dep11/ims/api/TeacherHttpController.java
@@ -1,10 +1,14 @@
 package lk.ijse.dep11.ims.api;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import lk.ijse.dep11.ims.to.TeacherTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PreDestroy;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,12 +16,41 @@ import java.util.List;
 @CrossOrigin
 @RequestMapping("/teachers")
 public class TeacherHttpController {
+    private final HikariDataSource pool;
+
+    public TeacherHttpController(){
+        HikariConfig config = new HikariConfig();
+        config.setUsername("root");
+        config.setPassword("root");
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/dep11_ims");
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        config.addDataSourceProperty("maximumPoolSize",10);
+        pool = new HikariDataSource(config);
+    }
+    @PreDestroy
+    public void destroy(){
+        pool.close();
+    }
+
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(produces = "application/json", consumes = "application/json")
-    public TeacherTO createTeacher(@RequestBody @Validated TeacherTO details){
-        System.out.println("Create Teacher");
-        return new TeacherTO();
+    public TeacherTO createTeacher(@RequestBody @Validated TeacherTO teacher) {
+        try (Connection connection = pool.getConnection()) {
+
+            PreparedStatement stm = connection.prepareStatement("INSERT INTO teacher (name, contact) VALUES (?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            stm.setString(1, teacher.getName());
+            stm.setString(2, teacher.getContact());
+            stm.executeUpdate();
+            ResultSet generatedKeys = stm.getGeneratedKeys();
+            generatedKeys.next();
+            int id = generatedKeys.getInt(1);
+            teacher.setId(id);
+            return teacher;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
